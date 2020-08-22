c9ac9c5017ea4aedb1a9dea0efb703ac056eb5dd
Areeb Khan <areebkhan619@gmail.com>
2020-08-22 21:27:03 +0530

    Added undo and reset game functionality.

--- TwentyFourtyEight.js
+++ TwentyFourtyEight.js
@@ -6,25 +6,15 @@ class TwentyFourtyEight extends Component {
     super(props);
     this.myRef = React.createRef();
     this.state = {
+      // matrix: [],
       matrix: [
-        [null, null, null,null],
-        [null, null, null,null],
-        [null, null, null,null],
-        [null, null, null,null]
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null],
+        [null, null, null, null]
       ],
-      lastMove: [
-        [null, null, null,null],
-        [null, null, null,null],
-        [null, null, null,null],
-        [null, null, null,null]
-      ]
-
-      // matrix: [
-      //   [2, 4, null, null],
-      //   [null, null, null, null],
-      //   [null, 512, 2048, null],
-      //   [4, 128, null, null],
-      // ],
+      prev: [],
+      score: 0
 
       // very important below
 
@@ -34,43 +24,35 @@ class TwentyFourtyEight extends Component {
       //   [null, 512, 2, null],
       //   [4, null, 2, null]
       // ]
-
-      // matrix: [
-      //   [4, null, 2, null],
-      //   [4, 128, null, null],
-      //   [null, 512, 2, null],
-      //   [2, 2, 4, 4]
-      // ]
-
-      // matrix: [
-      //   [4, null, 2, null],
-      //   [null, 128, null, 64],
-      //   [null, 512, null, null],
-      //   [2, 2, 4, 2]
-      // ]
     };
-  }
-
-  componentDidUpdate(old,niu){
-    console.log(old, niu)
   }
 
   componentDidMount() {
     this.myRef.current.focus();
-    let m = this.state.matrix.slice()
-    this.insertNew(m)
-    this.insertNew(m)
+    this.startOver()
+  }
+
+  startOver = () => {
+    let m = [
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null],
+      [null, null, null, null]
+    ];
+    this.insertNew(m, true);
+    this.insertNew(m, true);
     this.setState({
       matrix: m,
-      lastMove: m.slice()
-    })
+      prev: m,
+      score: 0,
+      optedForRestart: false
+    });
   }
 
   move = (m, el, dir) => {
     let r = el.rIndex;
     let c = el.cIndex;
     // let v = el.value;
-
 
     if (dir === "up" && r !== 0) {
       //shift logic
@@ -113,10 +95,10 @@ class TwentyFourtyEight extends Component {
   };
 
   merge = (mat, ind, dir) => {
-
     let m = mat.slice();
     let arrIndices = [];
     let t = [];
+    let score = this.state.score
 
     if (dir === "up" || dir === "down") {
       ind.map((currEl, i) => {
@@ -135,35 +117,32 @@ class TwentyFourtyEight extends Component {
               let next = t[i + 1];
               if (i !== lastIndex) {
                 if (next) {
-                    if (currEl.value === next.value) {
-                      m[index][currEl.cIndex] = 2 * currEl.value;
-                      m[currEl.rIndex][currEl.cIndex] = 2 * currEl.value; // might seem repetitive, but is needed
-                      m[next.rIndex][next.cIndex] = null;
-                      m[index + 1][currEl.cIndex] = null; // might seem repetitive, but is needed
-                      index = index + 1;
-                      lastIndex = i + 1;
-                    } else {
-                      m[index + 1][currEl.cIndex] = null;
-                      m[currEl.rIndex][currEl.cIndex]=null
-                      m[index][currEl.cIndex] = currEl.value;
-                      index = index + 1;
-                    }
+                  if (currEl.value === next.value) {
+                    m[index][currEl.cIndex] = 2 * currEl.value;
+                    m[currEl.rIndex][currEl.cIndex] = 2 * currEl.value; // might seem repetitive, but is needed
+                    score = score + 2*currEl.value
+                    m[next.rIndex][next.cIndex] = null;
+                    m[index + 1][currEl.cIndex] = null; // might seem repetitive, but is needed
+                    index = index + 1;
+                    lastIndex = i + 1;
+                  } else {
+                    m[index + 1][currEl.cIndex] = null;
+                    m[currEl.rIndex][currEl.cIndex] = null;
+                    m[index][currEl.cIndex] = currEl.value;
+                    index = index + 1;
+                  }
                 } else {
                   m[currEl.rIndex][currEl.cIndex] = null;
                   m[index][currEl.cIndex] = currEl.value;
 
-                  if(m[index+1]){
-                    m[index+1][currEl.cIndex] = null;
+                  if (m[index + 1]) {
+                    m[index + 1][currEl.cIndex] = null;
                   }
 
                   index = index + 1;
-                  lastIndex = null
+                  lastIndex = null;
                 }
               }
-            });
-
-            this.setState({
-              matrix: m
             });
           } else if (dir === "down") {
             let index = 3; //max-rows
@@ -175,6 +154,7 @@ class TwentyFourtyEight extends Component {
                 if (i !== lastIndex) {
                   if (currEl.value === next.value) {
                     m[index][currEl.cIndex] = 2 * currEl.value;
+                    score = score + 2*currEl.value
                     m[next.rIndex][next.cIndex] = null;
                     m[index - 1][currEl.cIndex] = null;
                     index = index - 1;
@@ -190,21 +170,16 @@ class TwentyFourtyEight extends Component {
                 if (i !== lastIndex) {
                   m[currEl.rIndex][currEl.cIndex] = null;
                   m[index][currEl.cIndex] = currEl.value;
-                  if(m[index-1]){
-                    m[index-1][currEl.cIndex] = null;
+                  if (m[index - 1]) {
+                    m[index - 1][currEl.cIndex] = null;
                   }
 
                   index = index - 1;
                 }
               }
             });
-
-            // this.setState({
-            //   matrix: m
-            // });
           }
         }
-        return null
       });
     } else if (dir === "right" || dir === "left") {
       if (dir === "right") {
@@ -213,6 +188,7 @@ class TwentyFourtyEight extends Component {
             if (j !== 0 && m[i][j]) {
               if (m[i][j] === m[i][j - 1]) {
                 m[i][j] = 2 * m[i][j];
+                score = score + 2*m[i][j]
                 m[i][j - 1] = null;
               }
             }
@@ -230,15 +206,16 @@ class TwentyFourtyEight extends Component {
           }
         }
 
-        this.setState({
-          matrix: m
-        });
+        // this.setState({
+        //   matrix: m
+        // });
       } else {
         for (let i = 0; i < m.length; i++) {
           for (let j = 0; j <= m[i].length - 1; j++) {
             if (j !== m[i].length && m[i][j]) {
               if (m[i][j] === m[i][j + 1]) {
                 m[i][j] = 2 * m[i][j];
+                score = score + 2*m[i][j]
                 m[i][j + 1] = null;
               }
             }
@@ -256,181 +233,207 @@ class TwentyFourtyEight extends Component {
           }
         }
 
-        this.setState({
-          matrix: m
-        });
+        // this.setState({
+        //   matrix: m
+        // });
+      }
+    }
+
+    return score
+  };
+
+  random = (min, max) => {
+    // min and max included
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  };
+
+  containsNull = array => {
+    let arr = array.map(subArray => {
+      let res;
+      res = subArray.some(value => {
+        return !value;
+      });
+      return res;
+    });
+    if (arr.includes(true)) {
+      return true;
+    } else {
+      return false;
+    }
+  };
+
+  // deepCompare = (m) => {
+
+  // }
+
+  insertNew = (m, init = false) => {
+    let flag = init
+      ? true
+      : JSON.stringify(m) !== JSON.stringify(this.state.matrix);
+    if (flag) {
+      if (this.containsNull(m)) {
+        let value = Math.random() < 0.9 ? 2 : 4;
+
+        let rowIndex = this.random(0, 3);
+        let colIndex = this.random(0, 3);
+        while (m[rowIndex][colIndex]) {
+          rowIndex = this.random(0, 3);
+          colIndex = this.random(0, 3);
+        }
+
+        m[rowIndex][colIndex] = value;
       }
     }
   };
 
-  random = (min, max) => { // min and max included 
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  }
-
-
-  // someFunction = (subArray) => {
-  //   let res
-  //   res = subArray.some((value)=>{
-  //     return !value
-  //   })
-  //   return res
-  //  }
-   
-   containsNull = (array) => {
-     let arr = array.map((subArray)=>{
-      let res
-      res = subArray.some((value)=>{
-        return !value
-      })
-      return res
-     })
-     if(arr.includes(true)){
-       return true
-     }
-     else{
-       return false
-     }
-   }
-
-  
-  
-
-  insertNew = (m) => {
-
-
-    if(this.containsNull(m)){
-      let value = Math.random() < 0.9 ? 2 : 4;
-
-      let rowIndex = this.random(0,3)
-      let colIndex = this.random(0,3)
-      while(m[rowIndex][colIndex]){
-        rowIndex = this.random(0,3)
-        colIndex = this.random(0,3)
-      }
-  
-      m[rowIndex][colIndex]=value
-      
-    }
- 
-  }
-
-
   undoAction = () => {
     this.setState({
-      matrix: this.state.lastMove
-    })
-  }
-
-  handleKeyDown = e => {
-    const { key } = e;
-
-    let ind = [];
-    this.state.matrix.map((row, i) => {
-      row.map((element, j) => {
-        if (element !== null) {
-          ind.push({ rIndex: i, cIndex: j, value: element });
-        }
-        return null
-      });
-      return null
+      matrix: this.state.prev
     });
+  };
 
+ 
 
-    let m = this.state.matrix.slice();
-    let lastMove =  this.state.matrix.slice()
+  handleKeyDown = async e => {
+    const { key } = e;
+    let score
+    if (
+      key === "ArrowLeft" ||
+      key === "ArrowRight" ||
+      key === "ArrowUp" ||
+      key === "ArrowDown"
+    ) {
+      let ind = [];
+      // let score = this.state.score
 
-    if (key === "ArrowLeft") {
-      ind.map(s => {
-        this.move(m, s, "left");
+      const clone = items =>
+        items.map(item => (Array.isArray(item) ? clone(item) : item));
+
+      let m = clone(this.state.matrix);
+
+      m.map((row, i) => {
+        row.map((element, j) => {
+          if (element !== null) {
+            ind.push({ rIndex: i, cIndex: j, value: element });
+          }
+        });
       });
 
-      this.merge(m, ind, "left");
+      let hasMatrixChanged;
 
-      this.insertNew(m)
-
-      this.setState({
-        lastMove,
-        matrix: m
-      });
-    }
-    if (key === "ArrowUp") {
-
-      ind.map(s => {
-        this.move(m, s, "up");
-      });
-
-      this.merge(m, ind, "up");
-
-      this.insertNew(m)
-
-
-      this.setState({
-        lastMove,
-        matrix: m
-      });
-    }
-    if (key === "ArrowRight") {
-
-      let colSorted = ind.slice().sort((a, b) => {
-        if (a.cIndex > b.cIndex) {
-          return -1;
-        } else if (a.cIndex < b.cIndex) {
-          return 1;
-        }
-      });
-
-      colSorted.map(s => {
-        this.move(m, s, "right");
-      });
-
-      this.merge(m, colSorted, "right");
-
-      // this.merge(m, ind, "right");
-
-      this.insertNew(m)
-
-
-      this.setState({
-        lastMove,
-        matrix: m
-      });
-    }
-    if (key === "ArrowDown") {
-
-      ind
-        .slice()
-        .reverse()
-        .map(s => {
-          this.move(m, s, "down");
+      if (key === "ArrowLeft") {
+        ind.map(s => {
+          this.move(m, s, "left");
         });
 
-      this.merge(m, ind.slice().reverse(), "down");
+        score = this.merge(m, ind, "left");
 
-      this.insertNew(m)
+        hasMatrixChanged =
+          JSON.stringify(m) !== JSON.stringify(this.state.matrix);
 
+        if (hasMatrixChanged) {
+          this.insertNew(m);
+        }
+      } else if (key === "ArrowUp") {
+        ind.map(s => {
+          this.move(m, s, "up");
+        });
 
-      this.setState({
-        lastMove,
-        matrix: m
-      });
+        score = this.merge(m, ind, "up");
+        hasMatrixChanged =
+          JSON.stringify(m) !== JSON.stringify(this.state.matrix);
+
+        if (hasMatrixChanged) {
+          this.insertNew(m);
+        }
+
+      } else if (key === "ArrowRight") {
+        let colSorted = ind.slice().sort((a, b) => {
+          if (a.cIndex > b.cIndex) {
+            return -1;
+          } else if (a.cIndex < b.cIndex) {
+            return 1;
+          }
+        });
+
+        colSorted.map(s => {
+          this.move(m, s, "right");
+        });
+
+        score = this.merge(m, colSorted, "right");
+
+        hasMatrixChanged =
+        JSON.stringify(m) !== JSON.stringify(this.state.matrix);
+
+      if (hasMatrixChanged) {
+        this.insertNew(m);
+      }
+
+      } else if (key === "ArrowDown") {
+        ind
+          .slice()
+          .reverse()
+          .map(s => {
+            this.move(m, s, "down");
+          });
+
+        score = this.merge(m, ind.slice().reverse(), "down");
+
+        hasMatrixChanged =
+          JSON.stringify(m) !== JSON.stringify(this.state.matrix);
+
+        if (hasMatrixChanged) {
+          this.insertNew(m);
+        }
+
+      }
+
+      if(hasMatrixChanged){
+        this.setState({
+          prev: this.state.matrix,
+          matrix: m,
+          score
+        });
+      }
     }
   };
 
   render() {
     return (
-      <div style={{ display: "flex", justifyContent: "center", alignItems: 'center', flexDirection: 'column' }}>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center"
+          // flexDirection: "column"
+        }}
+      >
+
+        <div>
+        <button onClick={this.undoAction} className="game-btn undo-btn" title="Undo last move" />
+        <button onClick={()=>this.setState({optedForRestart: true})} className="game-btn refresh-btn" title="Reset the game"/>
+        </div>
+
         <div
           ref={this.myRef}
           className="outer-box"
-          onKeyDown={this.handleKeyDown}
+          onKeyDown={e => this.handleKeyDown(e)}
           tabIndex="0"
         >
+          {this.state.isGameWon && <div className="game-won">Game Won!</div>}
 
-       {this.state.isGameWon && (
-          <div className="game-won">
-          Game Won!
-        </div>
-       )}
+          {this.state.isGameOver && <div className="game-won">Game Over!</div>}
+
+          {this.state.optedForRestart && 
+          <div className="game-won" style={{flexDirection: 'column'}}>
+            Restart?
+            <div style={{display: 'flex', fontSize: '50px', width: 'inherit', justifyContent: 'space-around'}}>
+              <div className="game-reset option" onClick={this.startOver}>Yes</div>
+              <div className="game-reset option" onClick={()=>this.setState({optedForRestart: false})}>No</div>
+            </div>
+            </div>
+          }
+
 
           {this.state.matrix.map((row, i) =>
             row.map((element, j) => (
@@ -452,12 +455,34 @@ class TwentyFourtyEight extends Component {
             ))
           )}
         </div>
-     
-        <button onClick={this.undoAction}>Undo</button>
-        <button onClick={()=>{this.setState({
-          isGameWon: true
-        })}}>Win Game</button>
 
+        <div
+          style={{
+            display: "flex",
+            flexDirection: "column",
+            backgroundColor: "grey",
+            margin: "10px",
+            color: "white",
+            borderRadius: "5px",
+            justifyContent: "center",
+            alignItems: "center"
+          }}
+        >
+          <div style={{ textAlign: "center", padding: "20px 5px" }}>
+            SCORE
+            <h3 style={{ margin: 0 }}>{this.state.score}</h3>
+          </div>
+          {/* <button onClick={this.undoAction}>Undo</button> */}
+          {/* <button
+            onClick={() => {
+              this.setState({
+                isGameWon: true
+              });
+            }}
+          >
+            Win Game
+          </button> */}
+        </div>
       </div>
     );
   }
